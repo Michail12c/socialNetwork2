1e4e3f518c78bad5830884d6bd023a1b9a6fc532
Michail Tsoma <dagobertusdagobertus@gmail.com>
2020-03-25 10:34:28 +0200

    refactoring API

--- my-app/src/components/api/api.ts
+++ my-app/src/components/api/api.ts
@@ -1,4 +1,5 @@
-import * as axios from 'axios';
+import { ProfileType } from './../../type';
+import axios from 'axios';
 
 const instance = axios.create({
   withCredentials: true,
@@ -15,30 +16,31 @@ export const userAPI = {
         return response.data;
       })
   },
-  disabled(userId){
+  disabled(userId: number){
       return  instance.delete(`follow/${userId}`)
   },
-  noDisabled(userId){
+  noDisabled(userId: number){
     return  instance.post(`follow/${userId}`)
   },
-  getProfile(userId){
+  getProfile(userId: number){
     console.warn('please, use profileAPI');
     return profileAPI.getProfile(userId);
   }
 }
+
 export const profileAPI = {
-  getProfile(userId){
+  getProfile(userId: number){
     return instance.get(`profile/` + userId)
   },
-  getProfileStatus (userId){
+  getProfileStatus (userId: number){
     return instance.get(`profile/status/` + userId)
   },
-  updateProfileStatus (status){
+  updateProfileStatus (status: string){
      return instance.put(`profile/status/`, {
        status: status
      })
   },
-  savePhoto(photoFile){
+  savePhoto(photoFile: any){
     const formData = new FormData();
     formData.append('image', photoFile);
     return instance.put('profile/photo/', formData, {
@@ -47,16 +49,36 @@ export const profileAPI = {
       }
     })
   },
-  saveProfile(profile){
+  saveProfile(profile: ProfileType){
     return instance.put(`profile`, profile)
   }
 }
+
+export enum ResultCodesEnum {
+  Success = 0,
+  Error = 1,
+  CaptchaIsRequired = 10
+}
+
+type MeResponseType  = {
+  data: {id: number, email: string, login: string}
+  resultCode: ResultCodesEnum
+  messages: Array<number>
+
+}
+
+type LoginMeResponseType  = {
+  data: {userId: number}
+  resultCode: ResultCodesEnum
+  messages: Array<number>
+
+}
 export const authAPI = {
   me (){
-    return instance.get(`auth/me`)
+    return instance.get<MeResponseType>(`auth/me`).then(res => res.data);
   },
-  login(email, password, rememberMe = false, captcha = null){
-    return instance.post('auth/login', {email, password, rememberMe, captcha})
+  login(email: string, password: string, rememberMe = false, captcha: null | string = null){
+    return instance.post<LoginMeResponseType>('auth/login', {email, password, rememberMe, captcha}).then(res => res.data)
   },
   logout(){
     return instance.delete('auth/login')
--- my-app/src/redux/auth-reducer.ts
+++ my-app/src/redux/auth-reducer.ts
@@ -1,5 +1,5 @@
 /* import { getCaptchaURLSuccess } from './auth-reducer'; */
-import { authAPI, securityAPI } from "../components/api/api";
+import { authAPI, securityAPI, ResultCodesEnum } from "../components/api/api";
 import { stopSubmit } from "redux-form";
 
 
@@ -55,30 +55,30 @@ export const getCaptchaURLSuccess = (captchaUrl: string): GetCaptchaURLSuccessTy
 }
 
 export const loginThunkCreator = () => async (dispatch: any) => {
-  let response = await authAPI.me() 
-      if(response.data.resultCode === 0){
-        let {id, email, login} =  response.data.data
+  let meData = await authAPI.me() 
+      if(meData.resultCode === ResultCodesEnum.Success){
+        let {id, email, login} =  meData.data
         dispatch(setUserData(id, email, login, true))
       }
 }
 
 export const login = (email: string, password: string, rememberMe: boolean, captcha: string) => async (dispatch: any) =>  { 
-  let response = await authAPI.login(email, password, rememberMe, captcha) 
-      if(response.data.resultCode === 0){
+  let loginData = await authAPI.login(email, password, rememberMe, captcha) 
+      if(loginData.resultCode === ResultCodesEnum.Success){
         dispatch(loginThunkCreator())
       }
       else {
-        if(response.data.resultCode === 10){
+        if(loginData.resultCode === ResultCodesEnum.CaptchaIsRequired){
           dispatch(getCaptchaURL());
         }
-        let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Somme error'; 
+        let message = loginData.messages.length > 0 ? loginData.messages[0] : 'Somme error'; 
         dispatch( stopSubmit('login', {_error: message}))
       }
 }
 
 export const logout = () => async (dispatch: any) => {
    let response = await authAPI.logout() 
-      if(response.data.resultCode === 0){
+      if(response.data.resultCode === ResultCodesEnum.Success){
         dispatch(setUserData(null, null, null, false))
       }
 }
